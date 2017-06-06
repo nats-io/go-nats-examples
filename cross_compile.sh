ea77f5724c81ccfb27085b718414d7e02b88aa05
Peter Miron <peter.miron@apcera.com>
2017-06-06 10:27:31 -0400

    minor changes to dir structure to upload full dir.

--- cross_compile.sh
+++ cross_compile.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 go get github.com/mitchellh/gox
-go get github.com/tcnksm/ghr
 
 export OUTDIR="release"
 
@@ -11,9 +10,11 @@ fi
 
 env CGO_ENABLED=0 gox -arch amd64 -output "release/{{.OS}}_{{.Arch}}/{{.Dir}}" $(go list ./... | grep -v "/vendor/")
 
+
 for dir in $(ls release); do \
+    (mkdir $OUTDIR/zips -p) ;\
     (cp LICENSE $OUTDIR/$dir/LICENSE) ;\
-    (cd $OUTDIR && zip -q go-nats-examples-$dir.zip -r $dir) ;\
-    echo "make $OUTDIR/go-nats-examples-$dir.zip" ;\
+    (cd $OUTDIR && zip -q zips/go-nats-examples-$dir.zip -r $dir) ;\
+    echo "make $OUTDIR/zips/go-nats-examples-$dir.zip" ;\
 done
 
