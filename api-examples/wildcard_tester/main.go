0ecf9fe466855f05f66c4b311073aa8e891c078e
Ivan Kozlovic <ivan@synadia.com>
2018-07-30 11:02:10 -0600

    Add wildcard tester
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

---
+++ api-examples/wildcard_tester/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/nats-io/go-nats"
+)
+
+func main() {
+	// [begin wildcard_tester]
+	nc, err := nats.Connect("nats://demo.nats.io:4222")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Close()
+
+	zoneID, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		log.Fatal(err)
+	}
+	now := time.Now()
+	zoneDateTime := now.In(zoneID)
+	formatted := zoneDateTime.String()
+
+	nc.Publish("time.us.east", []byte(formatted))
+	nc.Publish("time.us.east.atlanta", []byte(formatted))
+
+	zoneID, err = time.LoadLocation("Europe/Warsaw")
+	if err != nil {
+		log.Fatal(err)
+	}
+	zoneDateTime = now.In(zoneID)
+	formatted = zoneDateTime.String()
+
+	nc.Publish("time.eu.east", []byte(formatted))
+	nc.Publish("time.eu.east.warsaw", []byte(formatted))
+
+	// Close the connection
+	nc.Close()
+	// [end wildcard_tester]
+}
