a4c6ef97d0a3918669f31e82200bed1573656185
Ivan Kozlovic <ivan@synadia.com>
2018-07-29 19:58:24 -0600

    Add connection events/status
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

---
+++ api-examples/error_listener/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/nats-io/go-nats"
+)
+
+func main() {
+	// [begin error_listener]
+	// Set the callback that will be invoked when an asynchronous error occurs.
+	nc, err := nats.Connect("nats://demo.nats.io:4222",
+		nats.ErrorHandler(func(nc *nats.Conn, sub *nats.Subscription, err error) {
+			log.Printf("Error: %v", err)
+		}))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Close()
+
+	// Do something with the connection
+
+	// [end error_listener]
+}
---
+++ api-examples/servers_added/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/nats-io/go-nats"
+)
+
+func main() {
+	// [begin servers_added]
+	// Be notified if a new server joins the cluster.
+	// Print all the known servers and the only the ones that were discovered.
+	nc, err := nats.Connect("nats://demo.nats.io:4222",
+		nats.DiscoveredServersHandler(func(nc *nats.Conn) {
+			log.Printf("Known servers: %v\n", nc.Servers())
+			log.Printf("Discovered servers: %v\n", nc.DiscoveredServers())
+		}))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Close()
+
+	// Do something with the connection
+
+	// [end servers_added]
+}
