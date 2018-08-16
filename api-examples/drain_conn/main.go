1fe168ca38e203ffe409cc4e6f63ac00142273f6
Ivan Kozlovic <ivan@synadia.com>
2018-08-16 16:48:16 -0600

    Add Drain examples
    Signed-off-by: Ivan Kozlovic <ivan@synadia.com>

---
+++ api-examples/drain_conn/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"sync"
+	"time"
+
+	"github.com/nats-io/go-nats"
+)
+
+func main() {
+	// [begin drain_conn]
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	errCh := make(chan error, 1)
+
+	nc, err := nats.Connect("nats://demo.nats.io:4222",
+		nats.DrainTimeout(10*time.Second),
+		nats.ErrorHandler(func(_ *nats.Conn, _ *nats.Subscription, err error) {
+			errCh <- err
+		}),
+		nats.ClosedHandler(func(_ *nats.Conn) {
+			wg.Done()
+		}))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Close()
+
+	// Do something with the connection
+
+	// Drain the connection, which will close it when done.
+	if err := nc.Drain(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Wait for the connection to be closed.
+	wg.Wait()
+
+	// Check if there was an error
+	select {
+	case e := <-errCh:
+		log.Fatal(e)
+	default:
+	}
+
+	// [end drain_conn]
+}
---
+++ api-examples/drain_sub/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/nats-io/go-nats"
+)
+
+func main() {
+	// [begin drain_sub]
+
+	nc, err := nats.Connect("nats://demo.nats.io:4222")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Close()
+
+	done := sync.WaitGroup{}
+	done.Add(1)
+
+	count := 0
+	errCh := make(chan error, 1)
+
+	msgAfterDrain := "not this one"
+
+	// This callback will process each message slowly
+	sub, err := nc.Subscribe("updates", func(m *nats.Msg) {
+		if string(m.Data) == msgAfterDrain {
+			errCh <- fmt.Errorf("Should not have received this message")
+			return
+		}
+		time.Sleep(100 * time.Millisecond)
+		count++
+		if count == 2 {
+			done.Done()
+		}
+	})
+
+	// Send 2 messages
+	for i := 0; i < 2; i++ {
+		nc.Publish("updates", []byte("hello"))
+	}
+
+	// Call Drain on the subscription. It unsubscribes but
+	// wait for all pending messages to be processed.
+	if err := sub.Drain(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Send one more message, this message should not be received
+	nc.Publish("updates", []byte(msgAfterDrain))
+
+	// Wait for the subscription to have processed the 2 messages.
+	done.Wait()
+
+	// Now check that the 3rd message was not received
+	select {
+	case e := <-errCh:
+		log.Fatal(e)
+	case <-time.After(200 * time.Millisecond):
+		// OK!
+	}
+
+	// [end drain_sub]
+}
