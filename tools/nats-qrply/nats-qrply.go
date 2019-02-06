adb1d299a54e9fa9cdbb2a5f8e35822bac1032b5
Alberto Ricart <alberto@synadia.com>
2019-02-06 15:34:47 -0600

    Added goreleaser configuration so releases has pre-compiled binaries for macOS, windows, linux

--- tools/nats-qrply/nats-qrply.go
+++ tools/nats-qrply/nats-qrply.go
@@ -37,6 +37,7 @@ func printMsg(m *nats.Msg, i int) {
 func main() {
 	var urls = flag.String("s", nats.DefaultURL, "The nats server URLs (separated by comma)")
 	var userCreds = flag.String("creds", "", "User Credentials File")
+	var nkeyFile = flag.String("nkey", "", "NKey Seed File")
 	var showTime = flag.Bool("t", false, "Display timestamps")
 
 	log.SetFlags(0)
@@ -52,9 +53,22 @@ func main() {
 	opts := []nats.Option{nats.Name("NATS Sample Scalable Responder")}
 	opts = setupConnOptions(opts)
 
+	if *userCreds != "" && *nkeyFile != "" {
+		log.Fatal("specify -seed or -creds")
+	}
+
 	// Use UserCredentials
 	if *userCreds != "" {
 		opts = append(opts, nats.UserCredentials(*userCreds))
+	}
+
+	// Use Nkey authentication.
+	if *nkeyFile != "" {
+		opt, err := nats.NkeyOptionFromSeed(*nkeyFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		opts = append(opts, opt)
 	}
 
 	// Connect to NATS
